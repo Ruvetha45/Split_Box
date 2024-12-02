9490011b2d45cfd08b19f5f36f0bbe3fd0beb9af
Ruvetha45 <sruvetha8@gmail.com>
2024-12-02 22:48:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Box = ({ size }) => {
+  const [isSplit, setIsSplit] = useState(false);
+
+  const handleClick = (e) => {
+    e.stopPropagation(); // Prevent the click from bubbling up
+    setIsSplit(true);
+  };
+
+  if (!isSplit) {
+    return (
+      <div
+        className="box"
+        style={{ width: size, height: size }}
+        onClick={handleClick}
+      ></div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box-container" style={{ width: size, height: size }}>
+      <Box size={size / 2} />
+      <Box size={size / 2} />
+      <Box size={size / 2} />
+      <Box size={size / 2} />
     </div>
   );
-}
+};
+
+const App = () => {
+  const initialSize = 400;
+  return (
+    <div className="container-fluid">
+      <h1 className="header">SPLIT THE BOX</h1>
+      <Box size={initialSize} />
+    </div>
+  );
+};
 
 export default App;
